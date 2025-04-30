fa26f9de007a6e071177d1abbb741389bc2356dc
Pujanpoudel1 <puzanpoudel999@gmail.com>
2025-04-30 13:08:18 +0545

    Day 1 and Day 2

---
+++ day1.js
@@ -0,0 +1,33 @@
+console.log("Hello World!");
+console.log("First js project!");
+
+if (true) {
+  let name = "Pujan Poudel";
+  console.log(name);
+}
+
+// let name = "Pujan Poudel";
+// console.log(name);
+
+let name = "Pujan";
+console.log(name + " Poudel");
+
+let friend1 = "Pujn";
+let friend2 = "Salim";
+let friend3 = "Rahil";
+
+let friends = ["Pujn", "Salim", "Rahil", 12];
+console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends[2]);
+console.log(friends[3]);
+console.log(friends);
+
+let course = {
+  institute: "Digital Pathsala",
+  coursePrice: 999,
+  location: "Damak",
+  //key:value
+};
+console.log(course.coursePrice);
+console.log(course["location"]);
