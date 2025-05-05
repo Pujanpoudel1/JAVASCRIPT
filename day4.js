fe4b617c9881e265b9f4795b8a72a68d321eedfb
Pujanpoudel1 <puzanpoudel999@gmail.com>
2025-05-05 18:55:59 +0545

    Function Added

---
+++ day4.js
@@ -0,0 +1,47 @@
+// //functions 
+// console.log(1+1);
+// console.log(1+2);
+// console.log(1+3);
+
+// //regular function //function lai jaile pani invoke, call garnuparne huncha 
+// function add(a,b){ //parameters
+//     console.log(a+b)
+// }
+// add(1,4) //arguments
+// //functionle accept gareko{placeholder} kura chai parameters, argument bhaneko pathaune kura jun chai call garerw pathaincha.
+// function sum(a,b,c=3){
+//     console.log(a+b+c)
+// }
+// sum(1,2)
+// //named function
+// let addition = function(a,b,c){
+//     console.log(a+b+c)
+// }
+// addition(1,2,4)
+
+// // arrow function
+// let ADD = (a,b,c) => console.log(a+b+c)
+// ADD(1,2,5)
+
+// // anonymous function
+// // (()=>{
+// //     console.log("Hello World!")
+// // })()
+
+// //named arrow function
+// const anonymous =()=>{
+//     console.log("I am Anonymous.")
+// }
+// anonymous()
+
+
+//  let square =  function (num){
+//     return num*num
+//  }
+//  console.log(square(6))
+
+//square of a number using function
+const square =(num)=>{
+    console.log(num*num)
+}
+square(5)
